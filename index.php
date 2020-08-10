7605167e809866084d2da19dcee18e10a5ae4d7b
Kevin Moreno <strokers.supa@gmail.com>
2020-08-10 00:38:03 -0400

    Creacion del index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-4.5.1-dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="Estilos.css">
+    <link rel="stylesheet" href="Plugins\SweetAlert2\sweetalert2.min.css">
+    <title>Login</title>
+</head>
+<body>
+    
+
+
+    <script src="Jquery\jquery-3.5.1.min.js"></script>
+    <script src="bootstrap-4.5.1-dist\js\bootstrap.min.js"></script>
+    <script src="Popper\popper.min.js"></script>
+    <script src="Plugins\SweetAlert2\sweetalert2.all.min.js"></script>
+</body>
+</html>
